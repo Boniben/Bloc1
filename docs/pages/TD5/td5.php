fc25425cff31f0cf181dbf0614fa090d49e03cdb
Boniben <benjamin.boniface@sts-sio-caen.info>
2024-11-22 16:52:31 +0100

    début td5

---
+++ docs/pages/TD5/td5.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../../assets/css/TD5/td5.css">
+
+<?php
+$rien = isset($_GET);
+$message = isset($_GET['message']) ? $_GET['nom'] : 'Inconnu';
+
+echo "<p>'message par défaut'</p>";
+
+
+
+?> 
